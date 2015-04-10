4f0a56a5620824efec42ba22d16499373a8df518
Matt Nyman <matthew.nyman@yahoo.com>
2015-04-10 11:53:30 -0500

    Remove console debgging messages, change innerHTML to createTextNode

--- data/detector.js
+++ data/detector.js
@@ -3,10 +3,8 @@ var rickroll = "dQw4w9WgXcQ";
 
 for(var i=0; i<l.length; i++) {
   if(l[i].href.indexOf(rickroll) > -1) {
-    console.log("rickroll detected");
-    var text = document.createElement("span");
-    text.innerHTML = " (Rickroll detected)";
-    l[i].parentNode.insertBefore(text, l[i].nextSibling);
+    var text = document.createTextNode(" (Rickroll detected)");
+    l[i].appendChild(text);
   }
 }
 
