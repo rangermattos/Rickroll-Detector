2fd7741b09b4768d6996b95cabc773e8de736159
Matt Nyman <matthew.nyman@yahoo.com>
2015-04-09 23:47:49 -0500

    Added LICENSE, icon

--- data/detector.js
+++ data/detector.js
@@ -5,7 +5,7 @@ for(var i=0; i<l.length; i++) {
   if(l[i].href.indexOf(rickroll) > -1) {
     console.log("rickroll detected");
     var text = document.createElement("span");
-    text.innerHTML = "Rickroll detected";
+    text.innerHTML = " (Rickroll detected)";
     l[i].parentNode.insertBefore(text, l[i].nextSibling);
   }
 }
